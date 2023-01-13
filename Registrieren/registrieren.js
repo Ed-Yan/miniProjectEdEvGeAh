909747849ec5543ef2c2d98f3ac0c51337895f57
Ah-Jahan <ah.jahangiry@gmail.com>
2023-01-13 11:41:53 +0100

    logIn

---
+++ Registrieren/registrieren.js
@@ -0,0 +1,21 @@
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+const button = document.querySelector("button");
+
+
+
+function addUser () {
+    const user {
+        `email`: userName.value,
+        `password`: password.value,
+     }
+     console.log(user())
+
+}
+
+button.addEventListener("click", addUser);
+
+ 
+
+
+
