faddc5ac1c689fb6569da3f40ba0c2dee297650c
Ah-Jahan <ah.jahangiry@gmail.com>
2023-01-13 12:27:48 +0100

    anderung2

--- Registrieren/registrieren.js
+++ Registrieren/registrieren.js
@@ -1,21 +1,33 @@
 const userName = document.getElementById("username");
 const password = document.getElementById("password");
-const button = document.querySelector("button");
+const button = document.getElementById("button-style");
+let userOne = userName.value;
+let passwordOne = password.value;
 
+// function addUser() {
+//   const user = {
+//     email: userName.value,
+//     password: password.value,
+//   };
+//   console.log(user());
+// }
 
+button.addEventListener("click", create);
 
-function addUser () {
-    const user {
-        `email`: userName.value,
-        `password`: password.value,
-     }
-     console.log(user())
-
+async function create(userOne, passwordOne) {
+  const response = await fetch(
+    "https://miniprojectedevgeah-default-rtdb.firebaseio.com/login.json",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email: userOne,
+        password: passwordOne,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
 }
-
-button.addEventListener("click", addUser);
-
- 
-
-
-
